e2613b5fabb36ca9c3a46cb15f6388dfdca8c4b9
Arunava Chakraborty <144585145+Codewith-Arunava@users.noreply.github.com>
2026-07-08 21:30:28 +0530

    Update errorHandler.js

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -7,7 +7,7 @@ const errorHandler = (err, req, res, next) => {
     statusCode = 400;  
     message = `Invalid ID: ${err.value}`;
   }
-
+ 
   // Mongoose duplicate key
   if (err.code === 11000) {
     statusCode = 400;
